bcc66602a677b58fce51cf28341913d6819a670d
VegadelaLyra <lyradeperseo@gmail.com>
2022-12-07 15:02:00 -0500

    About to publish the package

--- bin/global.js
+++ bin/global.js
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
-#!/usr/bin/env node
-let beefy = require('../src/index.js')
-//// console.log(beefy) // ? { iLearned: [Function: iLearned] }
-const test = beefy.iLearned()
-test.newWord(
-=======
 #!sudo /usr/bin env node
-let beefy = require('../src/index.js')
+let grayMatter = require('../src/index.js')
 
+let beefy = iLearned()
 beefy.newWord(
->>>>>>> 66f86426a35e9087887e653679dd22fb9296fc23
     'beefy',
     'muscular or robust',
     'I have a beefy gf and a beefy laptop')
-test.anthology() // * beefy > muscular or robust > I have a beefy gf and a beefy laptop
+beefy.anthology() 
--- bin/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-#!sudo /usr/bin env node
-let grayMatter = require('../src/index.js')
-
-let beefy = iLearned()
-beefy.newWord(
-    'beefy',
-    'muscular or robust',
-    'I have a beefy gf and a beefy laptop')
-beefy.anthology() 
