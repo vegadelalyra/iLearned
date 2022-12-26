9e8c27a751c17b06235d450216cd448d518537fa
VegadelaLyra <lyradeperseo@gmail.com>
2022-12-25 21:47:56 -0500

    Learning how to deal with rl.history readLine node.js module method

--- src/assets/dependencies.mjs
+++ src/assets/dependencies.mjs
@@ -1,7 +1,5 @@
 import readline from 'readline'
 
-// readline node.js module: allows to input options.
 const rl = readline.createInterface(process.stdin, process.stdout) 
 
-// prompt: read user inputs, sigint: exits code with ^C, history: SAVES INPUTS INSIDE A FILE
 export default rl
--- src/assets/terminalConversation.mjs
+++ src/assets/terminalConversation.mjs
@@ -2,18 +2,21 @@ import rl from './dependencies.mjs'
 import { trollMessages } from './trollMessages.mjs'
 
 export default async function record(input = '') { 
+
     if (input === '') console.log("\nA'ight, let's fix your record!")
     
     const modifyValues = Object.assign({}, arguments)
     let [a, b, c] = [1, 1, 1] // independent counters for trollMessage output correctly on each case.
 
     switch (input.length) {
-
+        
         case 0:
             do {
                 trollMessages(a++)
                 arguments[1] = await new Promise( resolve => {
-                    rl.question('\x1b[33m> What concept or word have you learned?\n\x1b[37m ', resolve)
+                    rl.question('\x1b[33m> What concept or word have you learned?\n\x1b[37m ', input => {
+                        resolve(input)
+                    })
                     rl.write(modifyValues[1][1])
                 })
             } while (arguments[1].trim() === '')
@@ -31,30 +34,29 @@ export default async function record(input = '') {
             do {
                 trollMessages(c++)
                 arguments[3] = await new Promise( resolve => {
-                    rl.question('\n\x1b[33m> Give me an example [so we can say you have really got it... e_é]\n\x1b[37m', resolve )
+                    rl.question('\n\x1b[33m> Give me an example [\x1b[37mso we can say you have really got it... e_é\x1b[33m]\n\x1b[37m', resolve )
                     rl.write(modifyValues[1][3])
                 })
             } while (arguments[3].trim() === '')
 
         case 3:
+            console.log(rl.history)
             const chapter = `\n \x1b[33m{\x1b[37m ${arguments[1]} \x1b[33m / \x1b[37m ${arguments[2]} \x1b[33m / \x1b[37m ${arguments[3]} \x1b[33m}\x1b[37m`
             
-            console.log(chapter)
-            console.log(' '.repeat(69))
+            console.log(chapter, '\n', ' '.repeat(69))
             rl.question('\x1b[33mIs this right? \x1b[37m', answer => {
                 if (/^[^yos]/i.test(answer) || answer.length == 0) return record('', arguments)
                 
                 console.log('\nLearning...\n')
                 setTimeout( () => {
-                    console.log( `\x1b[33m¡¡¡ New knowledge successfully recorded !!! ^^\n ${chapter}\n` )
-                    console.log(' '.repeat(200))
+                    console.log( `\x1b[33m¡¡¡ New knowledge successfully recorded !!! ^^\n ${chapter}\n`, ' '.repeat(200) )
                 }, 1669 )
                 rl.close()
             }); rl.write('Yes')
             break
+            
         default:
             console.error(`\x1b[31merror! iLearned command can only run 3 arguments. You're running ${input.length} :(`)
             break
     }
-    // if (input.length < 3) return record(...arguments)
 }
