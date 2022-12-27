b427a2d82018fb52d85aa8c5b002e1ba80c47537
VegadelaLyra <lyradeperseo@gmail.com>
2022-12-26 22:58:05 -0500

    today command added

--- bin/input.mjs
+++ bin/input.mjs
@@ -1,1 +1,2 @@
 import Book from '../src/assets/saveQueue.mjs'
+Book.enqueue('[33m{[37m Hel asd [33m / [37m asff [33m / [37m fqef [33m}[37m')
--- src/yargs.mjs
+++ src/yargs.mjs
@@ -1,16 +1,18 @@
 #!/usr/bin/env node
 import yargs from 'yargs'
 import iLearned from './assets/iLearned.mjs'
+import '../bin/input.mjs'
+import Book from './assets/saveQueue.mjs'
 
 // YARGS: CLI flag (options) and commands arguments
 const argvInitialized = yargs(process.argv.slice(2)).argv
 const argv = yargs(process.argv.slice(2))
 .usage('\nRecord what you learn^^\niLearned [word] / [definition] / [example]\n"/" required ^')
 .command('$0', 'Record a new concept: word/def/exp', iLearned)
-.command('today', 'Show all concepts learned today.')
+.command('Helpa', 'Show all concepts learned today.', () => {console.log('FUCK U HELP')})
+.command('today', 'Show all concepts learned today.', ()=> console.log('\nToday I learned...\n', Book.show()) )
 .command('inLife', 'Show all concepts learned historycally.')
 .example(`hyphen / the "-" symbol / Required to run CLI options`)
-.help()
 .argv
 
 export { argv, argvInitialized }
