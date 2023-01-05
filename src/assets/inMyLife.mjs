f1297c9037277d74dda14b466a9e29906a16695d
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-05 04:30:45 -0500

    moving on

--- src/assets/inMyLife.mjs
+++ src/assets/inMyLife.mjs
@@ -2,9 +2,19 @@ import '../../bin/input.mjs'
 import Book from "./saveQueue.mjs";
 
 export default function inMyLife() {
+
+    const centerText = (text) => {
+        const terminalWidth = process.stdout.columns;
+        const textWidth = text.length;
+        const paddingWidth = Math.floor((terminalWidth - textWidth) / 2);
+        return '\n' + " ".repeat(paddingWidth) +text + " ".repeat(paddingWidth)
+      }
+      
+    const newConceptLine = '\x1b[34m' + '~'.repeat(process.stdout.columns)
+    
     console.log(
-        '\n\x1b[33mFrom this life I have learned:\n\n' + Book.show(), 
-        `\n\n\n\x1b[97m    *:・ﾟ✧ ＼（＾▽＾）／ ${Book.length == 0 ? 'ミ★ ...nothing xd ★彡' : ''}` 
+        newConceptLine + `\n\x1b[33m${centerText("From this life I have learned:")}\n\n\n` + Book.show(), 
+        `\n\n${newConceptLine}\n\n\n\x1b[33m    *:・ﾟ✧ ＼（＾▽＾）／ ${Book.length == 0 ? 'ミ★ ...nothing xd ★彡' : ''}` 
     ) 
     process.exit()
 }
--- src/assets/today.mjs
+++ src/assets/today.mjs
@@ -2,8 +2,10 @@ import '../../bin/input.mjs'
 import Book from "./saveQueue.mjs";
 
 export default function today() {
+    const newConceptLine = '~'.repeat(process.stdout.columns)
+    
     console.log(
-        '\nToday I learned:\n\n' + Book.today(), 
+        newConceptLine + '\nToday I learned:\n\n' + Book.today(), 
         `\n\n\n\x1b[33m    *:・ﾟ✧ ＼（^ ▽ ^）／ ${Book.today() == '' ? 'ミ★ ...nothing xd ★彡' : ''}` 
     ) 
     process.exit()
