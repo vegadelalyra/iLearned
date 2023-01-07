6eb1e378fd22aae419ef299f383b47fc187dc184
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-07 18:35:05 -0500

    Dealing with web scraping

--- src/assets/commands/remembering/webScraping.js
+++ src/assets/commands/remembering/webScraping.js
@@ -1,23 +1,32 @@
-import axios from 'axios'
 import cheerio from 'cheerio'
 
 const URL = "https://www.brainyquote.com/topics/remember-quotes"
 const baseURL = "https://www.brainyquote.com"
+const epistle = []
 
 async function getQuotes(URL) {
     try {
         const res = await fetch(URL)
         const html = await res.text()
         const $ = cheerio.load(html)
+
         const quotes = $(".oncl_q:nth-child(1) div")
 
-        quotes.each(function() {
+        quotes.each(function(n = 0) {
             let quote = $(this).text()
-            if (quote.length < 138) console.log(quote, quote.length)
+            if (quote.length < 138) epistle.push(quote)
+            console.log(epistle, n++)
         })
 
         const onNext = $(".page-item:nth-child(8) .page-link").attr("href")
         if ($(".disabled").text() !== '..Next') return getQuotes(baseURL + onNext)     
 
-    } catch (err) { console.error(err) }
+    } catch (err) { 
+        console.error(err) 
+    } finally { 
+
+        const miau = epistle[Math.floor(Math.random() * epistle.length)]
+        console.log(miau)
+
+    }
 }; getQuotes(URL)
