d49253e3fb8acf9a0fdd0e1e3f2aac7f8d85678f
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-07 08:43:58 -0500

    Web scraping working!

--- src/assets/commands/remembering/webScraping.js
+++ src/assets/commands/remembering/webScraping.js
@@ -7,9 +7,12 @@ async function getQuotes() {
     try {
         const res = await axios.get(URL)
         const $ = cheerio.load(res.data)
-        const quote = $("h1").text()
+        const quote = $(".oncl_q:nth-child(1) div")
 
-        console.log(quote)
+        quote.each(function() {
+            let miau = $(this).text()
+            if (miau.length < 138) console.log(miau, miau.length)
+        })
 
     } catch (err) { console.error(err) }
 }; getQuotes()
