5088d0a553e7a933107554bb03db9fd9f3923096
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-06 22:33:49 -0500

    Kicking off the modular approach

--- src/yargs.mjs
+++ src/yargs.mjs
@@ -8,14 +8,19 @@ import remembering from './assets/commands/remembering/remembering.mjs'
 
 // YARGS: CLI flag (options) and commands arguments
 const argv = yargs(process.argv.slice(2))
-.usage('\nRecord what you learn\niLearned [word] / [definition] / [example]\n/ required')
+.usage('\nRecord what you learn\n\niLearned [word] / [definition] / [example]')
+.usage('"/" forward slash required to record a new entry in one line')
+.usage('^^^')
 .command('$0', 'Record a new concept: word / def / exp', () => {}, iLearned )
 .command('today', 'Show all concepts learned today', today)
 .command('in my life', 'Show all concepts learned in life', inMyLife)
 .command('to forget', 'Delete concepts [pass its names]', toForget)
 .command('remembering', 'Recovers last deleted concept/s', remembering)
-.example(`iLearned heat hiːt / to make something warm / learn will heat your soul `)
 .help()
+.example(`\niLearned warm wɔːrm / to become less cold / learn warms your soul`)
+.example(`\niLearned scrape skreɪp \n/ to succeed in getting something`)
+.example(`/ iLearned scraping things`)
+.example(` `)
 .argv
 
 export { argv }
