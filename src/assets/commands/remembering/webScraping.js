671c27248df1a1285f1b1b62de8ec32a371df28d
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-07 22:52:52 -0500

    Improving web scraping

--- src/assets/commands/remembering/webScraping.js
+++ src/assets/commands/remembering/webScraping.js
@@ -4,30 +4,33 @@ const URL = "https://www.brainyquote.com/topics/remember-quotes"
 const baseURL = "https://www.brainyquote.com", epistle = []
 
 async function getQuotes(URL, words) {
-        const res = await fetch(URL)
-        const html = await res.text()
-        const $ = cheerio.load(html)
+    // fetch and parse the target website
+    const res = await fetch(URL)
+    const html = await res.text()
+    const $ = cheerio.load(html)
 
-        const quotes = $(".oncl_q:nth-child(1) div")
-        quotes.each(function() { 
-            const quote = $(this).text()
-            if (quote.split(' ').length < words) epistle.push(quote) 
-        })
+    // CSS-selector and filter of the desired HTML element
+    const quotes = $(".oncl_q:nth-child(1) div")
+    quotes.each(function() { 
+        const quote = $(this).text()
+        if (quote.split(' ').length < words) epistle.push(quote) 
+    })
 
-        let button = nth => $(`.page-item:nth-child(${nth}) .page-link`)
-        let curated = a => button(a).attr('href'), nextPage
-        console.log(URL);
-        button(8).text() === 'Next' 
-        ? nextPage = curated(8)
-        : nextPage = curated(9)
+    // pagination (code will scrape until the last page)
+    let button = nth => $(`. (page-item:nth-child(${nth}) .page-link`)
+    let curated = a => button(a).attr('href'), nextPage
 
-        if ($(".disabled").text() !== '..Next') return getQuotes(baseURL + nextPage)     
+    button(8).text() === 'Next' 
+    ? nextPage = curated(8)
+    : nextPage = curated(9)
+
+    if ($(".disabled").text() !== '..Next') return getQuotes(baseURL + nextPage)     
         
-        // console.log([].push(...epistle));
+    // console.log([].push(...epistle));
 
-        // const miau = epistle[Math.floor(Math.random() * epistle.length)]
-        // console.log(miau.length);
-        // return miau
+    // const miau = epistle[Math.floor(Math.random() * epistle.length)]
+    // console.log(miau.length);
+    // return miau
 
 }; const phrase = await getQuotes(URL, 40)
 // console.log(phrase)
