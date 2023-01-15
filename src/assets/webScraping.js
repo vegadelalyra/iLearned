9c5faec72f9bf8fe150a85d430704088008b2969
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-15 13:08:31 -0500

    Dealing with headless browsers

--- src/assets/webScraping.js
+++ src/assets/webScraping.js
@@ -10,12 +10,25 @@ export default async function webScrape(userInput) {
     const URL = `https://dictionary.cambridge.org/dictionary/english/${userInput}`
     
     // fetching and parsing website
-    const res = await fetch(URL)
-    const html = await res.text()
-    const $ = load(html) 
+    // Time testing
+    const start = performance.now()
+    const res = await axios.get(URL)
+    const end = performance.now()
+    const elapsedTime = end - start
+    console.log(`Fetching last: ${elapsedTime}ms`)
+    
+    const start2 = performance.now()
+    const $ = load(res.data) 
+    const end2 = performance.now()
+    const elapsedTime2 = end2 - start2
+    console.log(`Fetching last: ${elapsedTime2}ms`)
 
     // Getting word
+    const start3 = performance.now()
     const word = $('#cald4-1-1~ .dpos-h .dpos-h_hw').text()
     const c2 = $('#cald4-1-1~ .dsense_b .C2').text()
+    const end3 = performance.now()
+    const elapsedTime3 = end3 - start3
+    console.log(`Fetching last: ${elapsedTime3}ms`)
     return console.log(userInput, word, !!c2, c2)
 }
