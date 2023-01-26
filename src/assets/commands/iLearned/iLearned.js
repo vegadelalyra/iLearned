fb1087d3f6d2e38822a1e0976f67dc8dc614ae09
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-26 00:12:18 -0500

    AAAAAAAAAA

--- src/assets/commands/iLearned/iLearned.js
+++ src/assets/commands/iLearned/iLearned.js
@@ -14,8 +14,8 @@ export default async function iLearned(book = []) {
     .map(item => item.trim()) ?? []
 
     // User's input arranged in book schema: word / def / exp
-    const args = [input[0], input[1], input[2]]
-    let {word = '', def = '', exp = ''} = args
+    const args = [input[0] ?? '', input[1] ?? '', input[2] ?? '']
+    let [word, def, exp] = args; book = [word, def, exp]
     
     // Prompt for users when running default
     const newConceptLine = '~'.repeat(process.stdout.columns)
@@ -26,12 +26,20 @@ export default async function iLearned(book = []) {
     // Web scraping word from Cambridge dictionary
     let userInput = word.split(' ').length == 1 ? word : word.replace(' ', '-'), cambridge
     if (/^[a-zA-Z-]+$/.test(userInput)) cambridge = await webScrape(userInput, true)
-    if (!cambridge)
-    word = word + ' ' + cambridge.IPA
-    
 
-    // LET'S START THE PARTY !!!
+    // if available in Cambridge, fill book, else, use user's book
+    if (!cambridge) return terminal_conversation(input, ...book)
+   
+    // An english word! filling book for user
+    word = word + ' ' + cambridge.IPA    // word 
+    if (!exp) exp = cambridge.exp        // exp
     book = [word, def, exp]
-    console.log(book);
+    if (def) return terminal_conversation(input, ...book)
+    const lvlpos = !!cambridge.lvl 
+    ? `${cambridge.lvl} ${cambridge.PoS} ` 
+    : `${cambridge.PoS} `
+    def = lvlpos + cambridge.def        // def
+    book = [word, def, exp]
+    input.push(book[1], book[2])
     return terminal_conversation(input, ...book)
 }
