8793a4b003082614c4ddfc097682ad3fbeaa6645
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-25 21:00:56 -0500

    Appending scrapy web functionalities to iLearned UX

--- src/assets/commands/iLearned/iLearned.js
+++ src/assets/commands/iLearned/iLearned.js
@@ -1,9 +1,10 @@
 #!/usr/bin/env node
 import yargs from 'yargs'
 import terminal_conversation from './terminalConversation.js'
+import webScrape from '../../scrapyWeb/words.js'
 
 // my default option 
-export default function iLearned(book = []) {
+export default async function iLearned(book = []) {
     // Decoding user's input
     const argvInitialized = yargs(process.argv.slice(2)).argv
     const input = argvInitialized._
@@ -12,7 +13,7 @@ export default function iLearned(book = []) {
     .filter( el => { return el != null && el != '' } )
     .map(item => item.trim()) ?? []
 
-    // User's input arraged in book schema: word / def / exp
+    // User's input arranged in book schema: word / def / exp
     const args = [input[0] ?? '', input[1] ?? '', input[2] ?? '']
     let [word, def, exp] = args
     book = [word, def, exp]
@@ -23,9 +24,10 @@ export default function iLearned(book = []) {
     if (input.length < 4) console
     .log(`\x1b[33m${newConceptLine}\n\n\x1b[0m${newBookMsg} \x1b[33m ฅ ={^･ｪ･^}= \᳡ \n`)
 
-    // Web scraping from Cambridge
-    let cambridge
-    if (/^[a-zA-Z]+$/i.test(word)) console.log(word, 'is a solely word :D') 
+    // Web scraping word from Cambridge dictionary
+    let userInput = word.split(' ').length == 1 ? word : word.replace(' ', '-')
+    if (/^[a-zA-Z-]+$/.test(userInput)) await webScrape(userInput)
 
+    // LET'S START THE PARTY !!!
     return terminal_conversation(input, ...book)
 }
