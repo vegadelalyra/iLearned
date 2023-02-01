dca1b8f4ffff902710a06152ee38ce7527be14ac
VegadelaLyra <lyradeperseo@gmail.com>
2023-01-31 22:25:47 -0500

    Tinkering on one-line inputs

--- src/assets/dependencies/validation.js
+++ src/assets/dependencies/validation.js
@@ -2,20 +2,9 @@ import Book from "../saveQueue.js"
 
 export default function validateHashMap(values) {
   const hashMap = Book.hashMap
-  const lowerCaseHashMap = {}
-
-  // create a lower case hash map
-  Object.keys(hashMap).forEach(key => {
-    lowerCaseHashMap[key.toLowerCase()] = hashMap[key]
+  return hashMap.filter(book => {
+    values.join(' ')
+    .toLowerCase()
+    .includes(book.toLowerCase()) 
   })
-
-  // convert input values to lower case and filter out invalid values
-  const filteredValues = values
-    .map(value => value.toLowerCase())
-    .filter(value => lowerCaseHashMap[value])
-
-  // convert filtered values back to the original case and return
-  return filteredValues.map(value => 
-    Object.keys(hashMap).find(key => 
-      key.toLowerCase() === value))
 }
